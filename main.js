5e068ebd4304c3bd2c627e0075bb072c83944302
Maxwell Russell <maxruss.87@gmail.com>
2016-10-13 07:21:25 -0500

    adding in tab files

---
+++ main.js
@@ -0,0 +1,22 @@
+var tabs = document.querySelectorAll('.tabs');
+var textArea = document.querySelector('.text');
+var tabText = ['Red Wedding Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' , 'Blue Wedding Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' , 'Green Wedding Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'];
+var showContent = tabText[0];
+textArea.textContent = showContent;
+
+// console.log(tabs);
+// console.log(tabText);
+
+
+function clickHandler(e) {
+  // var showContent;
+  tabClicked = e.target.id - 1;
+  showContent = tabText[tabClicked];
+  textArea.textContent = showContent;
+  console.log('click');
+}
+
+for (var i = 0; i < tabs.length; i++) {
+	tabs[i].addEventListener('click', clickHandler);
+
+}
