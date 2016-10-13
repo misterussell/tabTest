db87021e1ae28f08a481857a9fbf312a4c1da465
Maxwell Russell <maxruss.87@gmail.com>
2016-10-13 08:36:58 -0500

    added in hash update

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ var textArea = document.querySelector('.text');
 var tabText = ['Red Wedding in the heat of the sahara. The bedouin tents scattered across the dunes. Dates and snakes centers of attention for the many gold clad guests.' , 'Blue Wedding in the sea with the beasts of the depths. Deep in a trench, the cool waters team with mer people and small blinking fish. Only sushi at this wedding buffet.' , 'Green Wedding with fairies and tree people, and ents alike. Running around under the Moon and the stars. Enjoy life.'];
 var showContent = tabText[0];
 textArea.textContent = showContent;
-
+var hashUpdate = document.location.hash;
+// console.log(hashUpdate);
 //Test validations
 // console.log(tabs);
 // console.log(tabText);
-
 
 function clickHandler(e) {
   tabClicked = e.target.id - 1;
   showContent = tabText[tabClicked];
   textArea.textContent = showContent;
+  hashUpdate = 'tab: '+ e.target.id;
+  //PROOF hash update
+  // console.log(hashUpdate);
   //PROOF CLICK
   // console.log('click');
 }
